9658f49919a03a1e734dd968ecd4f2ef84bb55fc
nuuuwan <nuuuwan@gmail.com>
2026-08-05 12:23:02 +0530

    Update stops.json with new coordinates for CTB Junction and Ja Ela; modify GeographicDiagram to draw a different route

--- src/lk_metro/GeographicDiagram.py
+++ src/lk_metro/GeographicDiagram.py
@@ -92,7 +92,7 @@ class GeographicDiagram:
 			f'<rect width="{self.width}" height="{self.height}" fill="#f7f5ef"/>',
 		]
 
-		routes_to_draw = [self.routes[3]]
+		routes_to_draw = [self.routes[4]]
 		for route in routes_to_draw:
 			points = " ".join(f"{x},{y}" for x, y in paths[route.id])
 			lines.append(
